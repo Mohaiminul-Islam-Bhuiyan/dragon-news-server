b4501d6c4937dbd7846b4e87045ccc72f9f82645
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-10-18 08:16:50 +0600

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require ('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+app.use(cors())
+
+const catagories = require('./data/catagories.json')
+
+app.get('/', (req, res) => {
+    res.send('news api is running')
+})
+
+app.get('/news-catagories', (req, res) => {
+    res.send(catagories)
+})
+
+app.listen(port, () => {
+    console.log('dragon news server running on port:', port)
+})
